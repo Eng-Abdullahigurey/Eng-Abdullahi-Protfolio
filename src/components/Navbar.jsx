9dd7a39bad56564e8813d1f2484682d7b7f11846
Eng Abdullahi <engabdullahi1313@gmail.com>
2025-09-19 18:36:56 -0700

    Build ready for Netlify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,46 @@
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-<ul className="nav-menu">
-  <li>
-    <Link to="hero" smooth={true} duration={400}>Home</Link>
-  </li>
-  <li>
-    <Link to="about" smooth={true} duration={400}>About</Link>
-  </li>
-  <li>
-    <Link to="services" smooth={true} duration={400}>Services</Link>
-  </li>
-  <li>
-    <Link to="projects" smooth={true} duration={400}>Projects</Link>
-  </li>
-  <li>
-    <Link to="contact" smooth={true} duration={400}>Contact</Link>
-  </li>
-</ul>
+const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavOpen(!navOpen);
+  };
+
+  const closeNav = () => {
+    setNavOpen(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">Eng Abdullahi </div>
+
+      {/* Desktop Menu */}
+      <ul className="nav-menu">
+        <li><Link to="hero" smooth={true} duration={500}>Home</Link></li>
+        <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+        <li><Link to="services" smooth={true} duration={500}>Services</Link></li>
+        <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
+        <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+      </ul>
+
+      {/* Mobile Hamburger */}
+      <div className="hamburger" onClick={toggleNav}>
+        {navOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* Mobile Menu */}
+      <ul className={`mobile-menu ${navOpen ? "open" : ""}`}>
+        <li><Link to="hero" smooth={true} duration={500} onClick={closeNav}>Home</Link></li>
+        <li><Link to="about" smooth={true} duration={500} onClick={closeNav}>About</Link></li>
+        <li><Link to="services" smooth={true} duration={500} onClick={closeNav}>Services</Link></li>
+        <li><Link to="projects" smooth={true} duration={500} onClick={closeNav}>Projects</Link></li>
+        <li><Link to="contact" smooth={true} duration={500} onClick={closeNav}>Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
